5389ae63da47aae2b256fe9326060c06e8291aba
Thalita Ribeiro <sribeiro.thalita@gmail.com>
2022-01-26 10:17:22 -0300

    Para o Spring disparar as validações do Bean Validation foi adicionado a anotação @Valid

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -3,7 +3,7 @@ package br.com.alura.forum.controller;
 import java.net.URI;
 import java.util.List;
 
-import br.com.alura.forum.form.TopicoForm;
+import br.com.alura.forum.controller.form.TopicoForm;
 
 import br.com.alura.forum.repository.CursoRepository;
 
@@ -21,6 +21,8 @@ import br.com.alura.forum.modelo.Topico;
 import br.com.alura.forum.repository.TopicoRepository;
 
 import org.springframework.web.util.UriComponentsBuilder;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/topicos")
@@ -44,7 +46,7 @@ public class TopicosController {
     }
 
     @PostMapping
-    public ResponseEntity<TopicoDto> cadastrar(@RequestBody TopicoForm form, UriComponentsBuilder uriBuilder) {
+    public ResponseEntity<TopicoDto> cadastrar(@RequestBody @Valid TopicoForm form, UriComponentsBuilder uriBuilder) {
         Topico topico = form.converter(cursoRepository);
         topicoRepository.save(topico);
 
